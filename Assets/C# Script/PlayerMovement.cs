93aa12e6913846e6c756e181bd2c54f3b22fe204
KaTSukiFireStar <gonin.flavien@gmail.com>
2024-05-15 15:53:22 +0200

    Fin des mouvements du personnage

---
+++ Assets/C# Script/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    public int speed = 1;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (movement.x != 0 || movement.y != 0)
+        {
+            rb.velocity = movement * speed;
+        }
+        
+    }
+}
