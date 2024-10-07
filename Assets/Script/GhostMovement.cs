9f2160b75fc9c240c92eeda063910c674771acdc
KaTSukiFireStar <78344193+KatSukiFireStar@users.noreply.github.com>
2024-10-07 09:49:36 +0200

    Create A*

--- Assets/Script/GhostMovement.cs
+++ Assets/Script/GhostMovement.cs
@@ -21,6 +21,8 @@ public class GhostMovement : MonoBehaviour
 	private GameStateEventSO gameStateEvent;
 	[SerializeField]
 	private GameObjectVector2EventSO gameOverEvent;
+	[SerializeField]
+	private IAType iaType;
 	
 	private Graph graph;
 	private Rigidbody2D rb;
@@ -63,6 +65,7 @@ public class GhostMovement : MonoBehaviour
 		{
 			endGame = false;
 			transform.position = defaultPosition;
+			FindDirection();
 		}else if (s.Value == GameState.Chasing)
 		{
 			chasing = true;
@@ -108,7 +111,14 @@ public class GhostMovement : MonoBehaviour
 		if (!dead)
 		{
 			Node destination = graph.GetNode(positionEvent.Value);
-			Vector2 dir = Dijkstra(source, destination);
+			Vector2 dir = new();
+			if (iaType == IAType.Dijkstra)
+			{
+				dir = Dijkstra(source, destination);
+			}else if (iaType == IAType.AStar)
+			{
+				dir = AStar(source, destination);
+			}
 			if (!chasing)
 			{
 				SetDirection(dir);
@@ -274,6 +284,118 @@ public class GhostMovement : MonoBehaviour
 		}
 		Vector2 dir = current.Position - source.Position;
 		return dir;
+	}
+
+	private Vector2 AStar(Node source, Node destination)
+	{
+		distances = new();
+		List<Node> next = new();
+		List<Node> visited = new();
+
+		foreach (Node node in graph.Nodes)
+		{
+			if (node == source) 
+				continue;
+			
+			distances[node.Position] = int.MaxValue;
+		}
+
+		distances[source.Position] = 0;
+		visited.Add(source);
+		
+		Node current = source;
+		foreach (Edge edge in graph.Edges)
+		{
+			if (edge.From == current)
+			{
+				if (!present(next, edge.To) && !present(visited, edge.To))
+				{
+					next.Add(edge.To);
+					distances[edge.To.Position] = distances[current.Position] + (int) Distance(edge.To.Position, destination.Position);
+				}
+			}
+			else if (edge.To == current)
+			{
+				if (!present(next, edge.From) && !present(visited, edge.From))
+				{
+					next.Add(edge.From);
+					distances[edge.From.Position] = distances[current.Position] + (int) Distance(edge.From.Position, source.Position) + (int) Distance(edge.From.Position, destination.Position);
+				}
+			}
+		}
+		
+		while (current.Position != destination.Position)
+		{
+			Node min = null;
+			int distMin = int.MaxValue;
+			foreach (Node node in next)
+			{
+				if (distances[node.Position] < distMin)
+				{
+					min = node;
+					distMin = distances[node.Position];
+				}
+			}
+			next.Remove(min);
+			visited.Add(min);
+			current = min;
+			foreach (Edge edge in graph.Edges)
+			{
+				if (edge.From == current)
+				{
+					if (!present(next, edge.To) && !present(visited, edge.To))
+					{
+						next.Add(edge.To);
+						distances[edge.To.Position] = distances[current.Position] + (int) Distance(edge.To.Position, destination.Position);
+						if (edge.To.Position == destination.Position)
+						{
+							distances[edge.To.Position] += 1;
+						}
+					}
+				}
+				else if (edge.To == current)
+				{
+					if (!present(next, edge.From) && !present(visited, edge.From))
+					{
+						next.Add(edge.From);
+						distances[edge.From.Position] = distances[current.Position] + (int) Distance(edge.From.Position, destination.Position);
+						if (edge.From.Position == destination.Position)
+						{
+							distances[edge.From.Position] += 1;
+						}
+					}
+				}
+			}
+		}
+
+		bool end = false;
+		while (!end)
+		{
+			foreach (Edge edge in graph.Edges)
+			{
+				if ((edge.To.Position == source.Position && edge.From.Position == current.Position) 
+				    || (edge.From.Position == source.Position && edge.To.Position == current.Position))
+				{
+					end = true;
+					break;
+				}
+				if(edge.To.Position == current.Position && distances[edge.From.Position] <= distances[current.Position])
+				{
+					current = edge.From;
+				}
+				if (edge.From.Position == current.Position && distances[edge.To.Position] <= distances[current.Position])
+				{
+					current = edge.To;
+				}
+			}
+		}
+		Vector2 dir = current.Position - source.Position;
+		return dir;
+	}
+
+	private float Distance(Vector2 source, Vector2 destination)
+	{
+		return Mathf.Abs(source.x - destination.x) + Mathf.Abs(source.y - destination.y);
 	}
 
 	private void SetDirection(Vector2 dir)
